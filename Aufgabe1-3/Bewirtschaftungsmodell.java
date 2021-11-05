2ae2a9a66cb8005aaa0dd643810b1d0c31be6d23
David <e11915769@student.tuwien.ac.at>
2021-11-05 12:20:41 +0100

    Fertig bis auf tostring

--- Aufgabe1-3/Bewirtschaftungsmodell.java
+++ Aufgabe1-3/Bewirtschaftungsmodell.java
@@ -4,7 +4,7 @@ public class Bewirtschaftungsmodell {
 
     /*
     wirtschaftsfaktoren[0] ist 1 wenn durch einen Kahlschlag geerntet werden soll ansonsten ist er 0
-    wirtschaftsfaktoren[1] ist ein Faktor in Prozent der sagt wie hoch der Anteil an Festmetern pro Altersstruktur maximal sein darf
+    wirtschaftsfaktoren[1] 1 wenn Plenterwirtschaft betrieben wird sonst 0
     wirtschaftsfaktoren[2] ist ein Faktor in Prozent der bestimmt ab wann im Mischwald begonnen werden muss Baumarten zu fällen
                             also bei Faktor 0,4 würde man sobald eine Baumart weniger als 40% der Festmeter im Wald ausmacht beginnen
                             die andere Baumart zu fällen sodass nach der Fällung wieder 50% Laub- sowie Nadelbäume im Wald sind
