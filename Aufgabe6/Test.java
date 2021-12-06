fdab2dd3359f5a8af716682a197bf620621f7a33
Annerose Tartler <annerose.tartler@gmail.com>
2021-12-06 16:42:10 +0100

    AB6: Test

--- Aufgabe6/Test.java
+++ Aufgabe6/Test.java
@@ -13,6 +13,7 @@ public class Test {
         Harvester wheelHarvester = new WheelHarvester(chopper1);
 
         Harvester strideHarvester = new StrideHarvester(shredder2);
+        //TEST ToDO
 
         //KOMMENTAR: Testen der Funktionalität der Harvester
         System.out.println("Harvester Checks:");
