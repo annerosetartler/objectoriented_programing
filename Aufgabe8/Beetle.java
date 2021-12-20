030d6a1c074ce93d88707a1f5a3ab5df4d512b23
Annerose Laura Tartler <Window control About Get support e1100892@student.tuwien.ac.at>
2021-12-20 17:55:24 +0100

    AB 8: comment

--- Aufgabe8/Beetle.java
+++ Aufgabe8/Beetle.java
@@ -4,7 +4,7 @@ public interface Beetle extends Runnable {
     //NACHB: beendet den Thread dieser Käferpopulation
     void endThread();
 
-    //NACHB: gibt true zurück, wenn diese Käferpopulation die Leibspeise von Ameisenbuntkäfern ist//TODO: zu humorvoll?
+    //NACHB: gibt true zurück, wenn diese Käferpopulation die Leibspeise von Ameisenbuntkäfern ist//TODO: zu humorvoll? vlt. nicht 100% treffend. Eher: Wenn die ABK sich daran nciht die zähne ausbeißen^^
     boolean isPrey();
 
     //NACHB: gibt den für die Käferpopulationsart charakteristischen String zurück
